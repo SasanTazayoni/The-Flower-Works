b16c629502a9131b71b96b7b5140d908dcc0ea81
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-05-12 12:16:50 +0100

    Fix broken scroll-to-top selector after data-scroll-up attribute removal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,46 @@
-const collapsibleToggler = document.querySelector('.collapsible__toggler');
-const collapsibleContent = document.querySelector('.collapsible__content');
-const btns = document.querySelectorAll('.button');
-const navbar = document.querySelector('[data-navbar]');
-const scrollToTop = document.querySelector('[data-scroll-up]');
+const collapsibleToggler = document.querySelector(".collapsible__toggler");
+const collapsibleContent = document.querySelector(".collapsible__content");
+const btns = document.querySelectorAll(".button");
+const navbar = document.querySelector("[data-navbar]");
+const scrollToTop = document.querySelector(".scroll-up-link");
 
 // Menu component
 
 if (collapsibleToggler && collapsibleContent) {
-    collapsibleToggler.addEventListener('click', () => {
-        collapsibleContent.classList.toggle('collapsible--expanded');
-    });
+  collapsibleToggler.addEventListener("click", () => {
+    collapsibleContent.classList.toggle("collapsible--expanded");
+  });
 }
 
 // Button component
 
-btns.forEach(btn => {
-    btn.addEventListener('mouseenter', function (e) {
-        let x = e.clientX - e.target.getBoundingClientRect().left;
-        let y = e.clientY - e.target.getBoundingClientRect().top;
-        let ripples = document.createElement('span');
-        ripples.style.left = x + 'px';
-        ripples.style.top = y + 'px';
-        this.appendChild(ripples);
+btns.forEach((btn) => {
+  btn.addEventListener("mouseenter", function (e) {
+    let x = e.clientX - e.target.getBoundingClientRect().left;
+    let y = e.clientY - e.target.getBoundingClientRect().top;
+    let ripples = document.createElement("span");
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+    this.appendChild(ripples);
 
-        setTimeout(() => {
-            ripples.remove();
-        }, 500);
-    });
+    setTimeout(() => {
+      ripples.remove();
+    }, 500);
+  });
 });
 
 // Scroll up link component
 
 if (navbar && scrollToTop) {
-    window.addEventListener('scroll', () => {
-        const scrollHeight = window.scrollY;
-        const navHeight = navbar.getBoundingClientRect().height;
-        if (scrollHeight > (navHeight * 2)) {
-            scrollToTop.classList.add('show-link');
-        } else {
-            scrollToTop.classList.remove('show-link');
-        }
+  window.addEventListener("scroll", () => {
+    const scrollHeight = window.scrollY;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight * 2) {
+      scrollToTop.classList.add("show-link");
+    } else {
+      scrollToTop.classList.remove("show-link");
+    }
 
-        scrollToTop.blur();
-    });
+    scrollToTop.blur();
+  });
 }
